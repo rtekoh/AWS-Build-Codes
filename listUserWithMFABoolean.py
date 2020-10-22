5b5d9ca951abb649c4bf18bb711a7b3056616e7e
rtekoh <rtekoh@outlook.com>
2020-10-21 22:05:34 -0400

    New aws codes

---
+++ listUserWithMFABoolean.py
@@ -0,0 +1,18 @@
+
+## this script will List users who have/haven't enabled MFA devices on IAM
+
+import boto
+from boto import iam
+
+connection=iam.IAMConnection()
+
+iamProfiles=connection.get_all_users();
+
+for user in iamProfiles.users:
+
+    mfaDevices = connection.get_all_mfa_devices(user.user_name)
+
+    if mfaDevices.mfa_devices:
+        print "User --->",user.user_name , " MFA : Enabled"
+    else:
+        print "User ---> ",user.user_name , "MFA : Disabled"
